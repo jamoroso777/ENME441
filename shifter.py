355d6a3001ae226f662a78d562d9cd555567deee
jamoroso777 <iamjoeyjoe777@gmail.com>
2025-10-12 16:57:11 -0400

    Add files via upload

--- shifter.py
+++ shifter.py
@@ -13,16 +13,16 @@ class Shifter:
     GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
     GPIO.setup(clockPin, GPIO.OUT, initial=0)  
 
-def ping(self, pin):
-  GPIO.output(pin, 1)
-  time.sleep(0)
-  GPIO.output(pin, 0)
+  def ping(self, pin):
+    GPIO.output(pin, 1)
+    time.sleep(0)
+    GPIO.output(pin, 0)
 
-def shiftByte(self, b):
-  for i in range(8):
-    GPIO.output(self.serialPin, b & (1<<i))
-    self.ping(self.clockPin)
-  self.ping(self.latchPin)
+  def shiftByte(self, b):
+    for i in range(8):
+      GPIO.output(self.serialPin, b & (1<<i))
+      self.ping(self.clockPin)
+    self.ping(self.latchPin)
 
 if __name__=="__main__":
   serialPin, latchPin, clockPin = 23, 24, 25
