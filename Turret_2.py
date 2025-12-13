2abeee9de85698339603c17d163a745ea047fbab
jamoroso777 <iamjoeyjoe777@gmail.com>
2025-12-13 17:41:18 -0500

    Update Turret_2.py

--- Turret_2.py
+++ Turret_2.py
@@ -32,7 +32,7 @@ DATA_PIN  = 16
 LATCH_PIN = 20
 CLOCK_PIN = 21
 
-USE_LOCAL_JSON = False
+USE_LOCAL_JSON = True
 LOCAL_JSON_FILE = "positions.json"
 JSON_URL = "http://192.168.1.254:8000/positions.json"
 
@@ -639,4 +639,5 @@ if __name__ == "__main__":
             pass
         GPIO.cleanup()
         print("GPIO cleaned up.")
+
 
